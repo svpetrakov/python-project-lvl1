4771396cce1e977220b68e0d1d589d3f998939f2
Sergei <sv.petrakov@yandex.ru>
2020-05-03 20:35:29 +0300

    fixed linter mistakes

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -5,6 +5,7 @@ def welcome_user():
     name = prompt.string('May I have your name? ')
     return name
 
+
 def check_parity_question():
     answer = prompt.string('Your answer: ')
     return answer
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,12 +5,14 @@ from random import randint
 
 def check_parity(name):
     '''
-    Функция генерирует случайное число в диапазоне от 1 до max_number, спрашивает пользователя 
-    является ли это число четным. 
-    Если пользователь отвечает не правильно - функция выводит сообщение о неправильном ответе и завершает игру.
-    Если пользователь отвечает правильно - функция выводит сообщение о правильном ответе и 
-    приступает к следующему числу. И так 3 раза.
-    Если игрок дал правильный ответ на все 3 вопроса, выводится поздравительное сообщение, игра завершается. 
+    Функция генерирует случайное число в диапазоне от 1 до max_number,
+    спрашивает пользователя является ли это число четным.
+    Если пользователь отвечает не правильно - функция выводит сообщение
+    о неправильном ответе и завершает игру.
+    Если пользователь отвечает правильно - функция выводит сообщение
+    о правильном ответе и приступает к следующему числу. И так 3 раза.
+    Если игрок дал правильный ответ на все 3 вопроса,
+    выводится поздравительное сообщение, игра завершается.
     '''
     max_number = 100
     question_number = 1
@@ -23,7 +25,8 @@ def check_parity(name):
                 question_number += 1
                 print('Correct!')
             else:
-                print("'"+ str(answer) + "' is wrong answer ;(. Correct answer was 'no'.")
+                print("'" + str(answer) + "' is wrong answer ;(.", end='')
+                print(" Correct answer was 'no'.")
                 print("Let's try again, " + str(name) + "!!")
                 return
         else:
@@ -31,7 +34,8 @@ def check_parity(name):
                 question_number += 1
                 print('Correct!')
             else:
-                print("'"+ str(answer) + "' is wrong answer ;(. Correct answer was 'yes'.")
+                print("'" + str(answer) + "' is wrong answer ;(.", end='')
+                print(" Correct answer was 'yes'.")
                 print("Let's try again, " + str(name) + "!!")
                 return
     print('Congratulations, ' + str(name) + '!')
@@ -43,7 +47,7 @@ def make_greeting():
     print('Answer "yes" if number even otherwise answer "no"')
     name = brain_games.cli.welcome_user()
     print('Hello, ' + str(name) + '!')
-    return(name)
+    return name
 
 
 def main():
